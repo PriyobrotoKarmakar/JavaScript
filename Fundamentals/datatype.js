0c241e460a84817ffd181ca9095e886538a41936
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2024-10-16 12:36:09 +0000

    update

--- Fundamentals/datatype.js
+++ Fundamentals/datatype.js
@@ -1,1 +1,12 @@
-consol.log("Priyo")
+console.log("priyo")
+let age = 56;
+const name = "Priyo";
+console.log(age)
+
+//in ls we can change the data
+// but in const we cannot change 
+age = 78;
+// name = "priyobroto";
+
+console.log(age)
+// console.log(name)
