66916f3971b5eec56bce9dcc11dbd67935d6dee7
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2024-10-16 20:09:02 +0530

    Update Operations.js

--- Fundamentals/Operations.js
+++ Fundamentals/Operations.js
@@ -1,3 +1,32 @@
-let val = -123;
-let negval = -val;
-console.log(negval)
+// let a = 5;
+// let negA = -a;
+// console.log(negA)
+
+// console.log(5+2)
+// console.log(5-2)
+// console.log(5*2)
+// console.log(5/2)
+// console.log(5**2)
+// console.log(5%2)
+
+// let fname  = "Priyo";
+// let lName = " Karmakar"
+// console.log(fname+lName)
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2 + 3);
+console.log(1 + 2 + "2" + 3);
+console.log(1 + 2 + 2 + "3");
+console.log(+true);
+console.log(+"")
+
+
+let n1,n2,n3;
+n1 = n2 = n3 = 2 + 2;
+console.log([n1,n2,n3])
+
+let gameCounter = 100;
+
+console.log([gameCounter,++gameCounter,gameCounter++])
+console.log(gameCounter)
